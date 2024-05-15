39dbeb4dda833dc3c21ca73f70bb5102c07e6d79
banksolo2 <seunolo2@gmail.com>
2024-05-15 18:13:22 +0100

    first commit
    This is my first commit

---
+++ src/test/java/com/bankstech/BankingSystemApplication/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankstech.BankingSystemApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
